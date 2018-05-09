7abe14e292ae4dfd45ec2089cf42f7b17baa4248
luhongalex <39117934+luhongalex@users.noreply.github.com>
2018-05-09 19:26:19 +0800

    第三次提交

--- app/src/main/java/com/example/dell/gittest/MainActivity.java
+++ app/src/main/java/com/example/dell/gittest/MainActivity.java
@@ -10,7 +10,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         //第三次提交
-        //
-//        提交成功
+        //提交成功
+
     }
 }
